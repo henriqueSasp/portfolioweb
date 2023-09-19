14dc0e488d7665de99d9212593027bcf0404e900
zeggy-t430 <henrique.sasp@gmai.com>
2023-09-19 07:20:55 -0300

    descobrindo como editar estilos com tailwindcss

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,7 @@
 export  function Header() {
   return (
     
-   <header className="fixed left-0 top-0 items-center flex w-full justify-between border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" >
+   <header className="padding: 20px, 20px; fixed left-0 top-0 items-center flex w-full justify-between border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" >
      <li> <a href="/">inicial</a> </li>
     <li> <a href="/home">home</a> </li>
     <li> <a href="/about">about</a> </li>
